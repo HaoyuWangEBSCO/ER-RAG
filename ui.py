fe582d795aa36d5c10835628f1eb46d88ab384ed
HaoyuWangEBSCO <146026568+HaoyuWangEBSCO@users.noreply.github.com>
2024-06-26 14:11:30 -0400

    Update ui.py

--- ui.py
+++ ui.py
@@ -59,7 +59,7 @@ def get_Ers_doc_based_on_topic(topic_list_string):
                        page_content=f"{item['name']}\n" + "\n".join(f"{output_dict.get(cv['id'], cv['id'])}: {cv['text'] or cv['value']}" for cv in item['column_values'] if cv['id'] in columns_to_keep),
                        metadata={cv['id']: cv['text'] for cv in item['column_values'] if cv['id'] in columns_to_keep})for item in data['data']['boards'][0]['items_page']['items']]
       elif "Filters" in topic_list_string:
-         API_KEY = st.secrets['apiKey']
+            API_KEY = st.secrets['apiKey']
             query=  """ {
                          boards (ids: 6633501571){
                            items_page (limit: 500) {
